42d7a76805b2fd907101e806d4b2374dfccea296
NSaiEPS <sainizameps@gmail.com>
2025-11-17 18:19:01 +0530

    version added

--- metlife/src/App.jsx
+++ metlife/src/App.jsx
@@ -9,6 +9,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { setRouter } from "./utils/navigate";
 function App() {
   setRouter(router);
+  console.log("v1.0.1");
   return (
     <>
       {/* <ToastBox /> */}
