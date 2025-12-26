857190c9abace2bff5780d07e19929a4673701c1
rishabhswami1 <rishabh_swami@eplanetsoft.com>
2025-12-26 10:24:23 +0530

    version changed

--- metlife/src/App.tsx
+++ metlife/src/App.tsx
@@ -8,7 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { setRouter } from "./utils/navigate";
 function App() {
   setRouter(router);
-  console.log("v1.0.7");
+  console.log("v1.0.8");
   return (
     <>
       {/* <ToastBox /> */}
